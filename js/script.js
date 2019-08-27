cc0c84ba96be17cb08070e2fbe17a5190d763a30
Edson Yogi <edson.yogi@hotmail.com>
2019-08-27 18:14:45 -0300

    add title

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 function initSectionActive () {
   const link = document.querySelectorAll('.item-link')
   const sections = document.querySelectorAll('.menu-section')
-  
+
   let y = 0
 
   link.forEach((itens) => {
@@ -14,26 +14,26 @@ function initSectionActive () {
     
     let x = 1
     link.forEach((links) => {
-      links.classList.remove('item-ativo')
+      links.classList.remove('item-active')
       if(links.getAttribute('href') === item.currentTarget.getAttribute('href')) {
-        links.classList.add('item-ativo')
+        links.classList.add('item-active')
         if(x > y) {
           y = x
           sections.forEach((elements) => {
-            elements.classList.remove('section-ativo-right')
-            elements.classList.remove('section-ativo-left')
+            elements.classList.remove('section-active-right')
+            elements.classList.remove('section-active-left')
             if(elements.id === item.currentTarget.getAttribute('href').split('#')[1]) {
-              elements.classList.add('section-ativo-right')
+              elements.classList.add('section-active-right')
               
             } 
           })
         }  else {
           y = x
           sections.forEach((elements) => {
-            elements.classList.remove('section-ativo-right')
-            elements.classList.remove('section-ativo-left')
+            elements.classList.remove('section-active-right')
+            elements.classList.remove('section-active-left')
             if(elements.id === item.currentTarget.getAttribute('href').split('#')[1]) {
-              elements.classList.add('section-ativo-left')
+              elements.classList.add('section-active-left')
             } 
           })
         }
@@ -55,11 +55,11 @@ function initMenuMobile() {
   menuMobileClose.addEventListener('click', desativarMenuMobile)
 
   function ativarMenuMobile() {
-      menu.classList.add('menu-mobile-ativo')
+      menu.classList.add('menu-mobile-active')
   }
 
   function desativarMenuMobile() {
-    menu.classList.remove('menu-mobile-ativo')
+    menu.classList.remove('menu-mobile-active')
   }
 }
 
